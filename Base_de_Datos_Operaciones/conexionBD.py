4d388c22e8dd4d0cd298e6bd4715bea8653506d7
camilodelgado23 <126426982+camilodelgado23@users.noreply.github.com>
2024-08-24 16:04:04 -0500

    add conexionBD function

---
+++ Base_de_Datos_Operaciones/conexionBD.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from mysql.connector import Error
+import sys
+import os
+
+# Añadir el directorio raíz al sys.path para permitir importaciones
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Importar las credenciales
+import credentials
+
+def create_connection():
+    """Create a database connection."""
+    try:
+        print("Attempting to connect to the database...")
+        connection = mysql.connector.connect(
+            host=credentials.DB_HOST,
+            user=credentials.DB_USER,
+            password=credentials.DB_PASSWORD,
+            database=credentials.DB_NAME
+        )
+        if connection.is_connected():
+            print('Connection to MySQL database established successfully.')
+            return connection
+        else:
+            print('Failed to connect to MySQL database.')
+            return None
+    except Error as e:
+        print(f'Error: {e}')
+        return None
+
+if __name__ == "__main__":
+    create_connection()
+
